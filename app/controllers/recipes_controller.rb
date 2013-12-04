a6b662d5c5f7312e09b23753445e41796a4f151d
Kerri Miller <kerrizor@kerrizor.com>
2013-12-04 14:52:20 -0800

    OMG IT WORKS... surprised?

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,6 +15,7 @@ class RecipesController < ApplicationController
   # GET /recipes/new
   def new
     @recipe = Recipe.new
+    @ingredients = Ingredient.all.collect { |p| [p.name, p.id] }
   end
 
   # GET /recipes/1/edit
@@ -25,6 +26,14 @@ class RecipesController < ApplicationController
   # POST /recipes.json
   def create
     @recipe = Recipe.new(recipe_params)
+
+    params[:recipe][:ingredients].each do |ingredient_id|
+      next if ingredient_id.to_i == 0
+
+      ingredient = Ingredient.find(ingredient_id.to_i)
+
+      @recipe.ingredients << ingredient
+    end
 
     respond_to do |format|
       if @recipe.save
@@ -62,13 +71,11 @@ class RecipesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def recipe_params
-      params.require(:recipe).permit(:name, :description, :process, :vegetarian)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :process, :vegetarian, :ingredients => {})
+  end
 end
